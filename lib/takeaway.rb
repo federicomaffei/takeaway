80136de8ba86278a3831da5a12064d233bbd2b29
federicomaffei <federico.maffei@gmail.com>
2014-06-08 18:36:34 +0100

    menu added

---
+++ lib/takeaway.rb
@@ -0,0 +1,23 @@
+class Takeaway
+
+	def initialize
+
+		@menu = [{dish: 'Pho', price: '7'}, 
+			{dish: 'Cha ca', price: '10'},
+			{dish: 'Banh xeo', price: '8'},
+			{dish: 'Cao lau', price: '12'},
+			{dish: 'Rau muong', price: '4'},
+			{dish: 'Nem ran', price: '9'},
+			{dish: 'Goi cuon', price: '5'},
+			{dish: 'Cha ca', price: '11'}]
+	
+	end
+
+	def menu
+
+		@menu
+
+	end
+
+
+end
